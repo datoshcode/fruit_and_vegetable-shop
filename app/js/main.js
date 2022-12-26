97954d2b5bd5ee4bb9b5d6d46a56a04e7c978147
Datosh <datoshcode@gmail.com>
2022-12-26 08:41:26 +0200

    added header__top

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,6 @@
-var languages = {
-  en: "English",
-  de: "Deutsch",
-};
-
-var DEFAULT_LANGUAGE = "en";
-
-var menuElem = document.getElementById("dropdown-menu");
-var titleElem = menuElem.querySelector(".title");
-
-// Set currently selected value (English in our case)
-function selectOption(optionName) {
-  titleElem.dataset.language = optionName;
-  titleElem.innerText = languages[optionName];
-}
-selectOption(DEFAULT_LANGUAGE);
-
-// Handle dropdown opening
-titleElem.onclick = function () {
-  menuElem.classList.toggle("open");
-};
-
-// Handle selecting new language
-var optionsHolter = menuElem.querySelector("ul");
-optionsHolter.onclick = function (event) {
-  menuElem.classList.remove("open");
-  console.log(event.target.dataset.language);
-  if (event.target.dataset && event.target.dataset.language) {
-    selectOption(event.target.dataset.language);
-  }
-};
+$(".dropdown__menu-inner").on("click", function () {
+  $(".dropdown__menu-list").toggleClass("menu__list--active");
+});
 
 // swiper
 var swiper = new Swiper(".comments__slider", {
